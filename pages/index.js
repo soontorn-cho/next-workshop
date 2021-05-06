8e4a3457a37a460d3c38cadbae62c474b67b7873
Soontorn Choosenpom <soontorn.cho@mail.pbru.ac.th>
2021-05-06 23:17:54 +0700

    แก้หน้าแรก ต้อนรับ นิดหน่อย

--- pages/index.js
+++ pages/index.js
@@ -13,11 +13,11 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to ยินดีต้อนรับสู่ <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          หน้าแรก สร้างโดย Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
